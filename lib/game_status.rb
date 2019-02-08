a3ed410cfb86d45478a4b20b9d290c0df292f28e
Christopher Luna <chris@illusionlayervfx.com>
2019-02-08 19:34:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -49,13 +49,16 @@ def over?(board)
 end
 
 def winner(board)
-
-  WIN_COMBINATIONS.find do |winner|
-  if won?(board) == winner && board[winner[0]] == "X"
-      binding.pry
-    return "X"
-  elsif won?(board) == winner && board[winner[0]] == "O"
-    return "O"
-  end
+if won?(board)
+  return board[won?(board)[0]]
+else
+  return nil
+  #WIN_COMBINATIONS.find do |winner|
+  #if won?(board) == winner && board[winner[0]] == "X"
+  #    binding.pry
+  #  return "X"
+  #elsif won?(board) == winner && board[winner[0]] == "O"
+  #  return "O"
+#  end
  end
 end
